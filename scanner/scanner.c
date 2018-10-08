a3dbfe9545edad1ced1bf87337f1ba5766651b9b
cmgrass <C.grass@live.com>
2018-10-07 21:16:21 -0400

    heading mag and true workgit add -A! yay

--- scanner/scanner.c
+++ scanner/scanner.c
@@ -7,6 +7,16 @@
 #include <unistd.h>
 #include "../include/util.h"
 
+/* DEBUG MACROS */
+/*
+#define CMG_DBG 1
+*/
+#ifdef CMG_DBG 
+#define CMG_PRINT(x) printf x
+#else
+#define CMG_PRINT(X) do {} while(0)
+#endif
+
 #define MAX_RECV_DGRAM 64
 
 void Usage(char *argv[])
@@ -26,6 +36,7 @@ int main(int argc, char *argv[])
   float myFloat[8];
   char dataStr[6];
   uint32_t floatInt;
+  uint32_t floatInt2;
 
   /*----- Validate Args -----*/
   if (argc != 2) {
@@ -65,45 +76,27 @@ int main(int argc, char *argv[])
     }
 
     printf("rcvd dgram from %s, len:%i\n", inet_ntoa(xplaneAddr.sin_addr), recvMsgLen);
-/*
-    printf("0:%.2f, 1:%.2f, 2:%.2f, 3:%.2f, 4:%.2f, 5:%.2f, 6:%.2f, 7:%.2f\n\n",
-           buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5],
-           buffer[6], buffer[7]);
-    printf("recvd: %s\n", buffer);
-*/
     strncpy(dataStr, buffer, 5);
-    myFloat[0] = (float)buffer[9];
-    myFloat[1] = (float)buffer[13];
-    myFloat[2] = (float)buffer[17];
-/*
-    myFloat[3] = (float)buffer[21];
-*/
-    myFloat[4] = (float)buffer[25];
-    myFloat[5] = (float)buffer[29];
-    myFloat[6] = (float)buffer[33];
-    myFloat[7] = (float)buffer[37];
-
     floatInt = 0;
-/*
-    floatInt = ((buffer[21]<<24) & (buffer[22]<<16) & (buffer[23]<<8) & (buffer[24]));
-*/
+
     floatInt = (uint32_t)buffer[24] << 24 |
                 (uint32_t)buffer[23] << 16 |
                 (uint32_t)buffer[22] << 8 |
                 (uint32_t)buffer[21];
+    floatInt2 = (uint32_t)buffer[20] << 24 |
+                (uint32_t)buffer[19] << 16 |
+                (uint32_t)buffer[18] << 8 |
+                (uint32_t)buffer[17];
+
 
     myFloat[3] = *((float *)&floatInt);
+    myFloat[2] = *((float *)&floatInt2);
 
-    printf("HEADER[%s, %x]\n", dataStr, (short)buffer[5]);
-    printf("HEADING: %hhx,%hhx,%hhx,%hhx\n", buffer[21], buffer[22], buffer[23], buffer[24]);
-    printf("HEADING: %x\n", floatInt);
-    printf("HEADING: %f\n", myFloat[3]);
-/*
-    printf("  f0: %.2f\n  f1:%.2f\n  f2:%.2f\n  f3:%.2f\n  f4:%.2f\n  f5:%.2f\n  f6:%.2f\n  f7:%.2f\n\n",
-           myFloat[0], myFloat[1], myFloat[2], myFloat[3], myFloat[4],
-           myFloat[5], myFloat[6], myFloat[7]);
-*/
-
+    CMG_PRINT(("HEADER[%s, %x]\n", dataStr, (short)buffer[5]));
+    CMG_PRINT(("HEADING mag: %hhx,%hhx,%hhx,%hhx\n", buffer[21], buffer[22], buffer[23], buffer[24]));
+    CMG_PRINT(("HEADING mag: %x\n", floatInt));
+    printf("HEADING mag: %f\n", myFloat[3]);
+    printf("HEADING true: %f\n", myFloat[2]);
   }
 
   return SUCCESS;
